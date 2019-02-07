2f52456ccbf160f7a252a7c8c08b2a5a9c0eb300
ZRunner <z.runner.mc@gmail.com>
2019-02-07 16:53:14 +0100

    [update] To version 3.3.4

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ author = 'ZRunner'
 # The short X.Y version
 version = '3.3'
 # The full version, including alpha/beta/rc tags
-release = '3.3.3'
+release = '3.3.4'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
