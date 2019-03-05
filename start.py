838fdba3fc54e8ad0abddf12e2fe25606d0ce5f7
ZRunner <z.runner.mc@gmail.com>
2019-03-05 01:05:32 +0100

    [remove] rss loop at launch

--- start.py
+++ start.py
@@ -249,9 +249,6 @@ def main():
         else:
             return
         if r in ['1','2']:
-            r2=input("Lancement de la boucle rss ? (o/n) ")
-            if r2=='o':
-                client.loop.create_task(client.cogs["RssCog"].loop())
             r3=input("Lancement de la boucle d'events' ? (o/n) ")
             if r3=='o':
                 client.loop.create_task(client.cogs["Events"].loop())
