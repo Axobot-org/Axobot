f64d6380f13aac9ec164e0ea4ba01948f7f393d0
ZRunner <z.runner.mc@gmail.com>
2019-03-25 20:33:42 +0100

    [update] members count when new guild

--- fcts/events.py
+++ fcts/events.py
@@ -48,10 +48,10 @@ class Events(commands.Cog):
         try:
             if Type == "join":
                 self.bot.log.info("Le bot a rejoint le serveur {}".format(guild.id))
-                desc = "Bot **joins the server** {} ({})".format(guild.name,guild.id)
+                desc = "Bot **joins the server** {} ({}) - {} users".format(guild.name,guild.id,len(guild.members))
             else:
                 self.bot.log.info("Le bot a quitté le serveur {}".format(guild.id))
-                desc = "Bot **left the server** {} ({})".format(guild.name,guild.id)
+                desc = "Bot **left the server** {} ({}) - {} users".format(guild.name,guild.id,len(guild.members))
             emb = self.bot.cogs["EmbedCog"].Embed(desc=desc,color=self.embed_colors['welcome']).update_timestamp().set_author(self.bot.user)
             await self.bot.cogs["EmbedCog"].send([emb])
         except Exception as e:
@@ -81,8 +81,6 @@ class Events(commands.Cog):
         
 
     async def send_mp(self,msg):
-        if "vient d'être ajouté dans la base de donnée" in msg.content:
-            return
         await self.check_mp_adv(msg)
         if msg.channel.recipient.id in [392766377078816789,279568324260528128,552273019020771358]:
             return
