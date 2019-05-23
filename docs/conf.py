80d0150765ca6d41b66dc80321a9d684044af38e
ZRunner <z.runner.mc@gmail.com>
2019-05-23 17:53:10 +0200

    [update] version 3.4.6

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,9 @@ copyright = '2018, ZRunner'
 author = 'ZRunner'
 
 # The short X.Y version
-version = '3.4'
+version = '3.6'
 # The full version, including alpha/beta/rc tags
-release = '3.4.5b'
+release = '3.4.6'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
