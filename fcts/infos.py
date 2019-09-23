45c3b396f8fbf224ffc9ec5da3abbd88d2f9906b
ZRunner <z.runner.mc@gmail.com>
2019-09-23 22:29:53 +0200

    [fix] `discordlinks`

--- fcts/infos.py
+++ fcts/infos.py
@@ -724,7 +724,7 @@ Available types: member, role, user, emoji, channel, server, invite, category"""
             l = await self.translate(ctx.channel,'infos','discordlinks')
             links = ["https://dis.gd/status","https://dis.gd/tos","https://dis.gd/report","https://dis.gd/feedback","https://support.discordapp.com/hc/articles/115002192352","https://discordapp.com/developers/docs/legal"]
             txt = "\n".join(['['+l[i]+']('+links[i]+')' for i in range(len(l))])
-            em = self.bot.cogs["EmbedCog"].Embed(desc=txt).update_timestamp().create_footer(ctx.author).discord_embed()
+            em = self.bot.cogs["EmbedCog"].Embed(desc=txt).update_timestamp().create_footer(ctx.author)
             await ctx.send(embed=em)
         else:
             txt = "\n".join([f'• {k}: <{v}>' for k,v in (await self.translate(ctx.channel,'infos','discordlinks')).items()])
