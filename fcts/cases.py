bfbd9cbe20bb58e1d0c40b573f2a72bcc6c72241
ZRunner <z.runner.mc@gmail.com>
2019-09-29 21:18:50 +0200

    [fix] edition of case with `'`

--- fcts/cases.py
+++ fcts/cases.py
@@ -159,7 +159,7 @@ class CasesCog(commands.Cog):
             raise ValueError
         cnx = self.bot.cnx_frm
         cursor = cnx.cursor()
-        query = ("""INSERT INTO `{}` (`ID`, `guild`, `user`, `type`, `mod`, `reason`,`duration`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}','{}')""".format(self.table,case.id,case.guild,case.user,case.type,case.mod,case.reason.replace("\'","\\\'"),case.duration))
+        query = ("""INSERT INTO `{}` (`ID`, `guild`, `user`, `type`, `mod`, `reason`,`duration`) VALUES ('{}', '{}', '{}', '{}', '{}', '{}','{}')""".format(self.table,case.id,case.guild,case.user,case.type,case.mod,case.reason.replace("'","\\'"),case.duration))
         cursor.execute(query)
         cnx.commit()
         return True
@@ -172,7 +172,7 @@ class CasesCog(commands.Cog):
             raise ValueError
         cnx = self.bot.cnx_frm
         cursor = cnx.cursor()
-        query = ("UPDATE `{}` SET `reason` = '{}' WHERE `ID` = {}".format(self.table,case.reason.replace("'","\\\'"),case.id))
+        query = ("UPDATE `{}` SET `reason` = '{}' WHERE `ID` = {}".format(self.table,case.reason.replace("'","\\'"),case.id))
         cursor.execute(query)
         cnx.commit()
         return True
