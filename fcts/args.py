b46661768e3461aa39f6be4d56aba0a6a69161cd
ZRunner <z.runner.mc@gmail.com>
2020-06-01 17:07:53 +0200

    [add] new timedelta format

--- fcts/args.py
+++ fcts/args.py
@@ -11,10 +11,14 @@ class tempdelta(commands.Converter):
         found = False
         # ctx.invoked_with
         for x in [('y',86400*365),('w',604800),('d',86400),('h',3600),('m',60)]:
-            r = re.search(r'(\d+)'+x[0],argument)
+            r = re.search(r'^(\d+)'+x[0]+'$',argument)
             if r!= None:
                 d += int(r.group(1))*x[1]
                 found = True
+        r = re.search(r'^(\d+)h(\d+)m?$', argument)
+        if r != None:
+            d += int(r.group(1))*3600 + int(r.group(2))*60
+            found = True
         if not found:
             raise commands.errors.BadArgument('Invalid duration: '+argument)
         return d
