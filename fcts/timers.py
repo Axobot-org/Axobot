14dee5b8cca7fe16f2ffd1cc3f878bce718a0ebc
ZRunner <z.runner.mc@gmail.com>
2020-07-02 00:37:17 +0200

    [add] `remindme` as alias of `reminder create`

--- fcts/timers.py
+++ fcts/timers.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import copy
+
 from fcts import args
 
 class TimersCog(commands.Cog):
@@ -15,21 +17,41 @@ class TimersCog(commands.Cog):
     async def on_ready(self):
         self.translate = self.bot.cogs["LangCog"].tr
 
-    
-    @commands.command(name="remindme", aliases=["remind", "reminds", "rmd"])
+    @commands.command(name="remindme")
     @commands.cooldown(5,30,commands.BucketType.channel)
     @commands.cooldown(5,60,commands.BucketType.user)
-    async def remindme(self, ctx:commands.Context, duration:commands.Greedy[args.tempdelta], *, message):
+    async def remindme(self, ctx: commands.Context, *, args):
+        """Create a new reminder
+        This is actually an alias of `reminder create`
+        """
+        ctx.message.content = ctx.prefix + "reminder create " + args
+        new_ctx = await self.bot.get_context(ctx.message)
+        await self.bot.invoke(new_ctx)
+    
+    
+    @commands.group(name="reminder", aliases=["remind", "reminds", "rmd"])
+    async def remind_main(self, ctx:commands.Context):
         """Ask the bot to remind you of something later
-Please use the following format:
-`XXm` : XX minutes
-`XXh` : XX hours
-`XXd` : XX days
-`XXw` : XX weeks
-
-..Example remindme 49d Think about doing my homework
 
 ..Doc miscellaneous.html#reminders"""
+        if ctx.subcommand_passed==None:
+            await self.bot.cogs['HelpCog'].help_command(ctx,['reminder'])
+    
+    
+    @remind_main.command(name="create", aliases=["add"])
+    @commands.cooldown(5,30,commands.BucketType.channel)
+    @commands.cooldown(5,60,commands.BucketType.user)
+    async def remind_create(self, ctx:commands.Context, duration:commands.Greedy[args.tempdelta], *, message):
+        """Create a new reminder
+        
+        Please use the following format:
+        `XXm` : XX minutes
+        `XXh` : XX hours
+        `XXd` : XX days
+        `XXw` : XX weeks
+
+        ..Example remindme create 49d Think about doing my homework
+        """
         duration = sum(duration)
         if duration < 1:
             await ctx.send(await self.translate(ctx.channel, "fun", "reminds-too-short"))
@@ -48,6 +70,14 @@ Please use the following format:
             await ctx.bot.get_cog("ErrorsCog").on_cmd_error(ctx,e)
         else:
             await ctx.send(await self.translate(ctx.channel, "fun", "reminds-saved", duration=f_duration))
+
+
+    @remind_main.command(name="list")
+    @commands.cooldown(5,60,commands.BucketType.user)
+    async def remind_list(self, ctx:commands.Context):
+        """List your pending reminders
+        """
+        pass
 
 
 def setup(bot: commands.Bot):
