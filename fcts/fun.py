3eef7ba02895b07b5da1d47d3c8ab5d8d0241efe
ZRunner <z.runner.mc@gmail.com>
2020-07-04 22:51:15 +0200

    [fix] misc. fixes

--- fcts/fun.py
+++ fcts/fun.py
@@ -94,7 +94,7 @@ class FunCog(commands.Cog):
     async def roll(self,ctx,*,options):
         """Selects an option at random from a given list
         The options must be separated by a semicolon `;`"""
-        liste = [x for x in [x.strip() for x in options.split(';')] if len(x)>0]
+        liste = list(set([x for x in [x.strip() for x in options.split(';')] if len(x)>0]))
         if len(liste) < 2:
             return await ctx.send(await self.translate(ctx.channel,"fun","no-roll"))
         choosen = None
@@ -635,7 +635,7 @@ You can specify a verification limit by adding a number in argument (up to 1.000
         await ctx.send(await self.translate(ctx.channel,'fun','markdown'))
     
 
-    @commands.command(name="bubble-wrap", aliases=["papier-bulle", "bbw"], hidden=True)
+    @commands.command(name="bubble-wrap", aliases=["papier-bulle", "bw"], hidden=True)
     @commands.cooldown(5,30,commands.BucketType.channel)
     @commands.cooldown(5,60,commands.BucketType.user)
     async def bubblewrap(self, ctx:commands.Context, width:int=10, height:int=15):
