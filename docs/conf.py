d48af4954d03ca22762aa6344111b178e6d88445
ZRunner <z.runner.mc@gmail.com>
2021-01-10 11:32:17 +0100

    [update] version 3.7.1 + copyrights

--- docs/conf.py
+++ docs/conf.py
@@ -19,13 +19,13 @@ sys.path.insert(0, os.path.abspath('.'))
 # -- Project information -----------------------------------------------------
 
 project = 'ZBot'
-copyright = '2019, ZRunner'
+copyright = '2019 - 2021, ZRunner'
 author = 'ZRunner'
 
 # The short X.Y version
 version = '3.7'
 # The full version, including alpha/beta/rc tags
-release = '3.7.0'
+release = '3.7.1'
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
