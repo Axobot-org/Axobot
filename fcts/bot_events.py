b65b95ed7e3ce990de9821aa0cad6a625247a4b9
ZRunner <z.runner.mc@gmail.com>
2021-05-30 19:33:31 +0200

    [fix] event end date

--- fcts/bot_events.py
+++ fcts/bot_events.py
@@ -27,7 +27,7 @@ class BotEvents(commands.Cog):
                 ev_data["begin"], "%Y-%m-%d")
             ev_data["end"] = datetime.datetime.strptime(
                 ev_data["end"], "%Y-%m-%d")
-            if ev_data["begin"].date() <= today and ev_data["end"].date() > today:
+            if ev_data["begin"].date() <= today and ev_data["end"].date() >= today:
                 self.current_event = ev_data["type"]
                 self.current_event_data = ev_data
                 self.current_event_id = ev_id
