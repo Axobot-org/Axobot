8f21ad455d7c0738c88819c1efeb5838c6aa6191
ZRunner <z.runner.mc@gmail.com>
2021-08-17 23:27:44 +0200

    [fix] some autocompletion

--- utils.py
+++ utils.py
@@ -17,6 +17,8 @@ class MyContext(commands.Context):
     """Replacement for the official commands.Context class
     It allows us to add more methods and properties in the whole bot code"""
 
+    bot: 'zbot'
+
     @property
     def bot_permissions(self) -> discord.Permissions:
         """Permissions of the bot in the current context"""
