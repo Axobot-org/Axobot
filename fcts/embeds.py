a43ef0607da6eeeefbf6b1de4ce6e0bce05b8794
ZRunner <z.runner.mc@gmail.com>
2022-02-07 09:59:46 +0100

    refractor(embeds): remove useless old class

--- fcts/embeds.py
+++ fcts/embeds.py
@@ -1,12 +1,11 @@
-import datetime
 from json.decoder import JSONDecodeError
+
 import discord
 import requests
-import typing
 from discord.ext import commands
-from libs.classes import Zbot, MyContext
+from libs.classes import Zbot
 
-url_base = 'https://discord.com/api/webhooks/'
+BASE_URL = 'https://discord.com/api/webhooks/'
 
 class Embeds(commands.Cog):
     """Cog for the management of the embeds. No more, no less."""
@@ -20,131 +19,28 @@ class Embeds(commands.Cog):
         }
         self.file = "embeds"
 
-
-    class Embed:
-        def __init__(self,title="",desc="",url="",color=None,time=discord.Embed.Empty,footer_url="",footer_text="",thumbnail="",image="",author_name="",author_url="",author_icon="",fields=None):
-            self.title = title
-            self.description = desc
-            self.url = url
-            self.color = color
-            self.timestamp = time
-            self.footer_text = footer_text
-            self.footer_url = footer_url
-            self.thumbnail = thumbnail
-            self.image = image
-            self.author_name = author_name
-            self.author_url = author_url
-            self.author_icon = author_icon
-            if fields is None:
-                fields = list()
-            for e,x in enumerate(fields):
-                if "inline" not in x.keys():
-                    fields[e]['inline'] = False
-                if "name" not in x.keys():
-                    fields[e]["name"] = "No name"
-                if "value" not in x.keys():
-                    fields[e]["value"] = "No value"
-            self.fields = fields
-
-        def add_field(self,name="No name",value="No value",inline=False):
-            self.fields.append({'name':name,'value':value,'inline':inline})
-
-        def update_timestamp(self):
-            self.timestamp = datetime.datetime.utcnow()
-            return self
-
-        def json(self):
-            j = dict()
-            emb = dict()
-            if self.title != "":
-                emb["title"] = self.title
-            if self.description != "":
-                emb["description"] = self.description
-            if self.url != "":
-                emb["url"] = self.url
-            if self.color is not None:
-                if isinstance(self.color,discord.Colour):
-                    emb["color"] = self.color.value
-                else:
-                    emb["color"] = self.color
-            if self.timestamp != discord.Embed.Empty:
-                emb["timestamp"] = str(self.timestamp)
-            if self.footer_text != "" or self.footer_url != "":
-                emb["footer"] = {"icon_url":str(self.footer_url),"text":self.footer_text}
-            if self.thumbnail != "":
-                emb["thumbnail"] = {"url": str(self.thumbnail)}
-            if self.image != "":
-                emb["image"]  = {"url": str(self.image)}
-            if self.author_icon != "" or self.author_name != "" or self.author_url != "":
-                auth = dict()
-                if self.author_name != "":
-                    auth["name"] = self.author_name
-                if self.author_url != "":
-                    auth["url"] = self.author_url
-                if self.author_icon != "":
-                    auth["icon_url"] = str(self.author_icon)
-                emb["author"] = auth
-            if self.fields != []:
-                emb["fields"] = self.fields
-            if emb != {}:
-                j["embed"] = emb
-            return j
-
-        def to_dict(self) -> dict:
-            return self.json()['embed']
-
-        def set_author(self, user: discord.User):
-            self.author_name = user.name
-            self.author_icon = user.avatar.with_static_format("png").with_size(256)
-            return self
-
-        async def create_footer(self, ctx:MyContext, user: typing.Union[discord.User,discord.Member]=None):
-            # self.footer_text = "Requested by {}".format(user.name)
-            if user is None:
-                user = ctx.author
-            self.footer_text = await ctx.bot._(ctx.channel, "misc.request_by", user=user.name)
-            self.footer_url = user.display_avatar.replace(format="png", size=256)
-            return self
-
-        def discord_embed(self):
-            if isinstance(self.color, discord.Colour):
-                color = self.color
-            elif self.color is None:
-                color = discord.Embed.Empty
-            else:
-                color = discord.Color(self.color)
-            emb = discord.Embed(title=self.title, colour=color, url=self.url, description=self.description, timestamp=self.timestamp)
-            emb.set_image(url=self.image)
-            emb.set_thumbnail(url=self.thumbnail)
-            emb.set_author(name=self.author_name, url=self.author_url, icon_url=str(self.author_icon))
-            emb.set_footer(text=self.footer_text, icon_url=str(self.footer_url))
-            for x in self.fields:
-                emb.add_field(name=x["name"],value=x["value"],inline=x["inline"])
-            return emb
-
-
     async def send(self, embeds, url: str=None):
         """Send a list of embeds to a discord channel"""
         if url is None:
-            url = url_base + self.logs['beta'] if self.bot.beta else url_base + self.logs['classic']
+            url = BASE_URL + self.logs['beta'] if self.bot.beta else BASE_URL + self.logs['classic']
         else:
             if url in self.logs.keys():
-                url = url_base + self.logs[url]
+                url = BASE_URL + self.logs[url]
         liste = list()
         for embed in embeds:
-            if isinstance(embed, (self.Embed, discord.Embed)):
+            if isinstance(embed, discord.Embed):
                 liste.append(embed.to_dict())
             else:
                 liste.append(embed["embed"])
         resp = requests.post(url, json={"embeds":liste})
         try:
-            msg = resp.json()
+            msg: dict = resp.json()
         except JSONDecodeError:
             return
         else:
             if "error" in msg.keys():
-                await self.bot.get_cog('Errors').senf_err_msg("`Erreur webhook {}:` [code {}] {}".format(url,resp.status_code,msg))
-
+                err_msg = "`Erreur webhook {}:` [code {}] {}".format(url,resp.status_code,msg)
+                await self.bot.get_cog('Errors').senf_err_msg(err_msg)
 
 
 def setup(bot):
