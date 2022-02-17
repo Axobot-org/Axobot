cc0a1bb03a6235264988f9431f06a83522d74796
ZRunner <z.runner.mc@gmail.com>
2022-02-17 21:50:16 +0100

    refractor(lint): spaces and things

--- fcts/languages.py
+++ fcts/languages.py
@@ -9,54 +9,53 @@ class Languages(discord.ext.commands.Cog):
         self.bot = bot
         self.file = "languages"
         self.languages = ('fr', 'en', 'lolcat', 'fi', 'de', 'fr2')
-        self.serv_opts = dict()
+        self.serv_opts: dict[int, str] = dict()
         i18n.set('filename_format', '{locale}.{format}')
         i18n.set('file_format', 'json')
         i18n.translations.container.clear()
         i18n.load_path.clear()
         i18n.load_path.append('./fcts/lang2')
 
-    async def tr(self, serverID, messageID: str, **args):
+    async def tr(self, server_id, string_id: str, **args):
         """Renvoie le texte en fonction de la langue"""
-        if isinstance(serverID,discord.Guild):
-            serverID = serverID.id
-        elif isinstance(serverID,discord.TextChannel):
-            serverID = serverID.guild.id
-        if str(serverID) in self.serv_opts.keys():
-            lang_opt = self.serv_opts[str(serverID)]
-            #print("Ex langage:",lang_opt)
-            #print(self.serv_opts)
+        if isinstance(server_id, discord.Guild):
+            server_id = server_id.id
+        elif isinstance(server_id, discord.TextChannel):
+            server_id = server_id.guild.id
+        if server_id in self.serv_opts.keys():
+            lang_opt = self.serv_opts[server_id]
         elif not self.bot.database_online:
             lang_opt = self.bot.get_cog('Servers').default_language
-        elif serverID is None:
+        elif server_id is None:
             lang_opt = self.bot.get_cog('Servers').default_language
-        elif isinstance(serverID,discord.DMChannel):
-            recipient = await self.bot.get_recipient(serverID)
+        elif isinstance(server_id,discord.DMChannel):
+            recipient = await self.bot.get_recipient(server_id)
             if recipient is None:
                 lang_opt = self.bot.get_cog('Servers').default_language
             else:
                 used_langs = await self.bot.get_cog('Utilities').get_languages(recipient, limit=1)
                 lang_opt = used_langs[0][0]
         else:
-            lang_opt = self.languages[await self.bot.get_config(serverID, "language")]
-            self.serv_opts[str(serverID)] = lang_opt
-            #print("New langage:",lang_opt)
+            lang_opt = self.languages[await self.bot.get_config(server_id, "language")]
+            self.serv_opts[server_id] = lang_opt
         if lang_opt not in self.languages:
             lang_opt = self.bot.get_cog('Servers').default_language
-        return await self._get_translation(lang_opt, messageID, **args)
-    
-    async def _get_translation(self, locale: str, messageID: str, **args):
-        if messageID == '_used_locale':
+        return await self._get_translation(lang_opt, string_id, **args)
+
+    async def _get_translation(self, locale: str, string_id: str, **args):
+        if string_id == '_used_locale':
             return locale
-        translation = i18n.t(messageID, locale=locale, **args)
-        if translation == messageID:
-            await self.msg_not_found(messageID, locale)
+        translation = i18n.t(string_id, locale=locale, **args)
+        if translation == string_id:
+            await self.msg_not_found(string_id, locale)
         return translation
 
-    async def msg_not_found(self, messageID: str, lang: str):
+    async def msg_not_found(self, string_id: str, lang: str):
+        "Signal to the dev that a translation is missing"
         try:
-            await self.bot.get_cog('Errors').senf_err_msg("Le message {} n'a pas été trouvé dans la base de donnée! (langue {})".format(messageID,lang))
-        except:
+            await self.bot.get_cog('Errors').senf_err_msg(
+                "Le message {} n'a pas été trouvé dans la base de donnée! (langue {})".format(string_id, lang))
+        except: # pylint: disable=bare-except
             pass
 
     async def check_tr(self, channel: discord.TextChannel, lang: str, origin: str="fr"):
@@ -94,11 +93,11 @@ class Languages(discord.ext.commands.Cog):
             else:
                 await channel.send(">> {} messages non traduits en `{}`".format(count,lang))
 
-    async def change_cache(self,serverID,new_lang):
+    async def change_cache(self, server_id: int, new_lang: str):
         #print("change_cache:",new_lang)
         if new_lang in self.languages:
             #print("changement effectué")
-            self.serv_opts[str(serverID)] = new_lang
+            self.serv_opts[server_id] = new_lang
 
 
 def setup(bot):
