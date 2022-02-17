0690c1656f90e70293c5810d52fb096f7afdbff1
ZRunner <z.runner.mc@gmail.com>
2022-02-17 18:28:50 +0100

    feat(modlogs): add beginning of modlogs feature

--- fcts/errors.py
+++ fcts/errors.py
@@ -142,6 +142,10 @@ class Errors(commands.Cog):
             r = re.search(r'Invalid card style: (\S+)',raw_error)
             if r is not None:
                 return await ctx.send(await self.bot._(ctx.channel,'errors.invalidcardstyle', s=r.group(1)), allowed_mentions=ALLOWED)
+             # Invalid server log type
+            r = re.search(r'Invalid server log type',raw_error)
+            if r is not None:
+                return await ctx.send(await self.bot._(ctx.channel,'errors.invalidserverlog'), allowed_mentions=ALLOWED)
             self.bot.log.warning('Unknown error type -',error)
         elif isinstance(error,commands.errors.MissingRequiredArgument):
             await ctx.send(await self.bot._(ctx.channel,'errors.missingargument',a=error.param.name,e=random.choice([':eyes:','',':confused:',':thinking:',''])))
