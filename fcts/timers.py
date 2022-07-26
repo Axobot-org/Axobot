ab68ca206576f46c4fa5dec64b26ec728a5023ae
ZRunner <z.runner.mc@gmail.com>
2022-07-26 23:53:27 +0200

    feat(reminders): delete nice select menu

--- fcts/timers.py
+++ fcts/timers.py
@@ -1,5 +1,6 @@
 import copy
 import datetime
+from typing import Optional
 
 import discord
 from discord.ext import commands
@@ -8,11 +9,61 @@ from libs.formatutils import FormatUtils
 
 from fcts import args, checks
 
+class ReminderSelectView(discord.ui.View):
+    "Used to ask to select a reminder to delete"
+    def __init__(self, reminders: list[dict], placeholder: str):
+        super().__init__()
+        options = self.build_options(reminders)
+        self.select = discord.ui.Select(placeholder=placeholder, min_values=1, max_values=len(reminders), options=options)
+        self.select.callback = self.callback
+        self.add_item(self.select)
+        self.reminders: list[str] = []
+    
+    def build_options(self, reminders: list[dict]):
+        "Build the options list for Discord"
+        res = []
+        for reminder in reminders:
+            if len(reminder['message']) > 90:
+                reminder['message'] = reminder['message'][:89] + '…'
+            label = reminder['message']
+            desc = f"{reminder['tr_channel']} - {reminder['tr_duration']}"
+            res.append(discord.SelectOption(value=reminder['id'], label=label, description=desc))
+        return res
+    
+    async def callback(self, interaction: discord.Interaction):
+        "Called when the dropdown menu has been validated by the user"
+        self.reminders = self.select.values
+        await interaction.response.defer()
+        self.select.disabled = True
+        await interaction.edit_original_message(view=self)
+        self.stop()
 
 class Timers(commands.Cog):
     def __init__(self, bot: Zbot):
         self.bot = bot
         self.file = "timers"
+    
+    async def db_get_reminder(self, reminder_id: int, user: Optional[int] = None) -> Optional[dict]:
+        if user is not None:
+            query = "SELECT * FROM `timed` WHERE user=%s AND action='timer' AND ID=%s"
+            args = (user, reminder_id)
+        else:
+            query = "SELECT * FROM `timed` WHERE action='timer' AND ID=%s"
+            args = (reminder_id, )
+        async with self.bot.db_query(query, args, fetchone=True) as query_result:
+            return query_result
+    
+    async def db_get_user_reminders(self, user: int) -> list[dict]:
+        "Get every active user reminder"
+        query = "SELECT * FROM `timed` WHERE user=%s AND action='timer'"
+        async with self.bot.db_query(query, (user,)) as query_results:
+            return query_results
+
+    async def db_delete_reminder(self, reminder_id: int, user: int):
+        "Delete a reminder for a user"
+        query = "DELETE FROM `timed` WHERE user=%s AND action='timer' AND ID=%s"
+        async with self.bot.db_query(query, (user, reminder_id), returnrowcount=True) as query_result:
+            return query_result > 0
 
     @commands.command(name="remindme", aliases=['rmd'])
     @commands.cooldown(5,30,commands.BucketType.channel)
@@ -112,31 +163,77 @@ class Timers(commands.Cog):
             text = "**"+await self.bot._(ctx.channel, "timers.rmd.title")+"**\n\n".join(liste)
             await ctx.send(text)
 
+    async def ask_reminder_ids(self, input_id: Optional[int], ctx: MyContext, title: str) -> Optional[list[int]]:
+        "Ask the user to select reminder IDs"
+        selection = []
+        if input_id is not None:
+            input_reminder = await self.db_get_reminder(input_id, ctx.author.id)
+            if not input_reminder:
+                input_id = None
+            else:
+                selection.append(input_reminder["ID"])
+        if input_id is None:
+            reminders = await self.db_get_user_reminders(ctx.author.id)
+            if len(reminders) == 0:
+                await ctx.send(await self.bot._(ctx.channel, "timers.rmd.empty"))
+                return
+            reminders_data: list[dict] = []
+            lang = await self.bot._(ctx.channel, '_used_locale')
+            for reminder in reminders[:25]:
+                rmd_data = {
+                    "id": reminder["ID"],
+                    "message": reminder["message"]
+                }
+                # channel name
+                if channel := self.bot.get_channel(reminder["channel"]):
+                    rmd_data["tr_channel"] = "#" + channel.name
+                else:
+                    rmd_data["tr_channel"] = reminder["channel"]
+                # duration
+                end: datetime.datetime = reminder["begin"] + datetime.timedelta(seconds=reminder['duration'])
+                end = end.astimezone(datetime.timezone.utc)
+                duration = await FormatUtils.time_delta(ctx.bot.utcnow(), end, lang=lang, year=True, form="short")
+                rmd_data["tr_duration"] = duration
+                # append to the list
+                reminders_data.append(rmd_data)
+            form_placeholder = await self.bot._(ctx.channel, 'timers.rmd.select-placeholder')
+            view = ReminderSelectView(reminders_data, form_placeholder)
+            await ctx.send(title, view=view)
+            await view.wait()
+            if view.reminders is None:
+                return
+            try:
+                selection = list(map(int, view.reminders))
+            except ValueError:
+                selection = []
+        if len(selection) == 0:
+            await ctx.send(await self.bot._(ctx.guild, "rss.fail-add"))
+            return
+        return selection
+
     @remind_main.command(name="delete", aliases=["remove", "del"])
     @commands.cooldown(5, 30, commands.BucketType.user)
-    async def remind_del(self, ctx: MyContext, ID: int):
+    async def remind_del(self, ctx: MyContext, ID: Optional[int] = None):
         """Delete a reminder
         ID can be found with the `reminder list` command.
 
-        ..Example rmd delete 253
+        ..Example reminders delete
+
+        ..Example reminders delete 253
 
         ..Doc miscellaneous.html#delete-a-reminder
         """
-        query = f"SELECT ID, message FROM `timed` WHERE user={ctx.author.id} AND action='timer' AND ID={ID}"
-        async with self.bot.db_query(query, fetchone=True) as query_results:
-            if not query_results:
-                await ctx.send(await self.bot._(ctx.channel, "timers.rmd.empty"))
-                return
-            item = query_results
-        ctx2 = copy.copy(ctx)
-        ctx2.message.content = item["message"]
-        item["message"] = (
-            await commands.clean_content(fix_channel_mentions=True).convert(ctx2, item["message"])
-            ).replace("`", "\\`")
-        await self.bot.task_handler.remove_task(item["ID"])
-        await ctx.send(await self.bot._(ctx.channel, "timers.rmd.deleted", id=item["ID"], message=item["message"]))
+        ids = await self.ask_reminder_ids(ID, ctx, await ctx.bot._(ctx.channel, "timers.rmd.delete.title"))
+        if ids is None:
+            return
+        count = 0
+        for reminder_id in ids:
+            if await self.db_delete_reminder(reminder_id, ctx.author.id):
+                await self.bot.task_handler.remove_task(reminder_id)
+                count += 1
+        await ctx.send(await self.bot._(ctx.channel, "timers.rmd.delete.success", count=count))
 
-    @remind_main.command(name="clear")
+    @remind_main.command(name="clear", enabled=False)
     @commands.cooldown(3, 60, commands.BucketType.user)
     async def remind_clear(self, ctx: MyContext):
         """Remove every pending reminder
