0671a9cc9f7cdb5f08bf5cd654ba133cd3a4281a
ZRunner <z.runner.mc@gmail.com>
2022-11-02 22:55:26 -0400

    refract(antiscam): use websites list from database

--- libs/antiscam/similarities.py
+++ libs/antiscam/similarities.py
@@ -1,48 +1,35 @@
-import csv
 from difflib import SequenceMatcher
-import os
 from re import finditer
 
 from .normalization import normalize_unicode
-
-WHITELISTED_WEBSITES: list[str] = []
-BLACKLISTED_WEBSITES: list[str] = []
-
-with open(os.path.dirname(__file__)+'/data/base_websites.csv', 'r', encoding='utf-8') as csv_file:
-    spamreader = csv.reader(csv_file)
-    for row in spamreader:
-        if row[0] == '1':
-            BLACKLISTED_WEBSITES.append(row[1])
-        else:
-            WHITELISTED_WEBSITES.append(row[1])
 
 
 def _similar(input_1: str, input_2: str):
     "Compare two strings and output the similarity ratio"
     return SequenceMatcher(None, input_1, input_2).ratio()
 
-def check_message(message: str) -> int:
+def check_message(message: str, websites_reference: dict[str, bool]) -> int:
     "Check every URL in a message and return the sum of each URL's score"
     score = 0
     message = normalize_unicode(message)
     for link in search_links(message):
         link = ".".join(link.split('.')[-2:])
-        score += check_url_similarity(link)
+        score += check_url_similarity(link, websites_reference)
     return max(score, 0)
 
-def check_url_similarity(url: str):
+def check_url_similarity(url: str, websites_reference: dict[str, bool]):
     "Check an URL similarity to known safe or dangerous websites"
     matching_score = 0
     url = url.replace(' ', '').lower().strip()
     # print('ANALYZING', url)
     if url == 'discord.gg':
         return -2
-    if url in WHITELISTED_WEBSITES:
+    if websites_reference.get(url, False):
         return - 10
     if url in {'bit.ly', 'cutt.ly', 'tinyurl.com'}:
         return 1
 
-    for sim in BLACKLISTED_WEBSITES:
+    for sim in (key for key, value in websites_reference.items() if not value):
         if url == sim:
             matching_score += 5
             break
@@ -61,7 +48,7 @@ def check_url_similarity(url: str):
     if matching_score > 1:
         return matching_score
 
-    for sim in WHITELISTED_WEBSITES:
+    for sim in (key for key, value in websites_reference.items() if value):
         seq = _similar(url, sim)
         # print('  SIMILAR AT', round(seq,3), 'TO', sim)
         if seq >= 0.85:
@@ -77,6 +64,23 @@ def search_links(message: str):
     return map(lambda x: x.group(1), finditer(r'([.\w_-]+\.\w{2,7})(/[/\S]{3,})?\b', message))
 
 if __name__ == '__main__':
+    websites_refs = {
+        'discord.gg': True,
+        'discord.com': True,
+        'discordapp.com': True,
+        'discord.me': True,
+        'discord.io': True,
+        'twitter.com': True,
+        'youtube.com': True,
+        'youtu.be': True,
+        'twitch.tv': True,
+        'discord.store': False,
+        'discord.gifts': False,
+        'discord.net': False,
+        'steamnitro.co': False,
+        'free-nitro.ru': False,
+        'discord-beta.com': False,
+    }
     print(check_message("""Free discord nitro for 1 month!
-https://discord-gg.com/nitro @everyone"""))
-    print(check_message("'@everyone 🤩Hey, steam gived nitro - https://dicsord.club/classic"))
+https://discord-gg.com/nitro @everyone""", websites_refs))
+    print(check_message("'@everyone 🤩Hey, steam gived nitro - https://dicsord.club/classic", websites_refs))
