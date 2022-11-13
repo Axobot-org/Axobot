3572ed1fda2d2b108caf8bd2bb5db5837d17d4f5
ZRunner <z.runner.mc@gmail.com>
2022-11-12 19:20:01 -0500

    feat(twitch): add logic commands

--- libs/twitch/api_types.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal, TypedDict, Any
-
-class HttpTokenNotSet(Exception):
-    pass
-
-class TokenResponse(TypedDict):
-    access_token: str
-    expires_in: int
-    token_type: Literal["bearer"]
