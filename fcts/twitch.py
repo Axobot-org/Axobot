fdedf5a11f088bb7194c43578b844ea3dde657e8
ZRunner <z.runner.mc@gmail.com>
2022-11-12 19:20:01 -0500

    feat(twitch): add logic commands

--- fcts/twitch.py
+++ fcts/twitch.py
@@ -1,11 +1,13 @@
-import json
+from typing import Optional
+from dateutil.parser import isoparse
 
+import discord
 from discord import app_commands
 from discord.ext import commands
 
 from libs.bot_classes import PRIVATE_GUILD_ID, MyContext, Zbot
 from libs.twitch.api_agent import TwitchApiAgent
-
+from libs.twitch.types import PlatformId, StreamersDBObject
 
 class Twitch(commands.Cog):
     "Handle twitch streams"
@@ -26,7 +28,45 @@ class Twitch(commands.Cog):
         "Close the Twitch session"
         await self.agent.close_session()
         self.bot.log.info("[twitch] connection closed")
-    
+
+    async def db_add_streamer(self, guild_id: int, platform: PlatformId, user_id: str, user_name: str):
+        "Add a streamer to the database"
+        query = "INSERT INTO `streamers` (`guild_id`, `platform`, `user_id`, `user_name`, `beta`) VALUES (%s, %s, %s, %s, %s)"
+        async with self.bot.db_query(query, (guild_id, platform, user_id, user_name, self.bot.beta), returnrowcount=True) as query_result:
+            return query_result > 0
+
+    async def db_get_guild_streamers(self, guild_id: int, platform: Optional[PlatformId]=None) -> list[StreamersDBObject]:
+        "Get the streamers for a guild"
+        query = "SELECT * FROM `streamers` WHERE `guild_id` = %s AND `beta` = %s"
+        args = [guild_id, self.bot.beta]
+        if platform is not None:
+            query += " AND `platform` = %s"
+            args.append(platform)
+        async with self.bot.db_query(query, args) as query_result:
+            return query_result
+
+    async def db_get_streamers(self, platform: Optional[PlatformId]=None) -> list[StreamersDBObject]:
+        "Get all streamers objects"
+        query = "SELECT * FROM `streamers` WHERE `beta` = %s"
+        args = [self.bot.beta]
+        if platform is not None:
+            query += " AND `platform` = %s"
+            args.append(platform)
+        async with self.bot.db_query(query, args) as query_result:
+            return query_result
+
+    async def db_remove_streamer(self, guild_id: int, platform: PlatformId, user_id: str):
+        "Remove a streamer from the database"
+        query = "DELETE FROM `streamers` WHERE `guild_id` = %s AND `platform` = %s AND `user_id` = %s AND `beta` = %s"
+        async with self.bot.db_query(query, (guild_id, platform, user_id, self.bot.beta), returnrowcount=True) as query_result:
+            return query_result > 0
+
+    async def db_set_streamer_status(self, platform: PlatformId, user_id: str, is_streaming: bool):
+        "Set the streaming status of a streamer"
+        query = "UPDATE `streamers` SET `is_streaming` = %s WHERE `platform` = %s AND `user_id` = %s AND `beta` = %s"
+        async with self.bot.db_query(query, (is_streaming, platform, user_id, self.bot.beta), returnrowcount=True) as query_result:
+            return query_result > 0
+
     @commands.hybrid_group(name="twitch")
     @app_commands.guilds(PRIVATE_GUILD_ID)
     @app_commands.default_permissions(manage_guild=True)
@@ -36,17 +76,82 @@ class Twitch(commands.Cog):
         if ctx.invoked_subcommand is None:
             await ctx.send_help(ctx.command)
 
-    @twitch.command(name="test")
-    async def test_twitch(self, ctx: MyContext, *user: str):
-        "Test the Twitch API"
-        resp = await self.agent.get_user_stream(*user)
-        await ctx.send(json.dumps(resp, indent=4))
+    @twitch.command(name="subscribe")
+    async def twitch_sub(self, ctx: MyContext, streamer: str):
+        "Subscribe to a twitch user"
+        user = await self.agent.get_user_by_name(streamer)
+        if user is None:
+            await ctx.send("User not found")
+            return
+        if await self.db_add_streamer(ctx.guild.id, "twitch", user["id"], user["display_name"]):
+            await ctx.send(f"Subscribed to channel {user['display_name']}")
+        else:
+            await ctx.send("Channel already subscribed!")
 
-    @twitch.command(name="status")
-    async def ping_twitch(self, ctx: MyContext):
-        "Check the twitch connection"
-        await ctx.send(f"""{self.agent.is_token_valid = }
-{self.agent.is_connected = }""")
+    @twitch.command(name="unsubscribe")
+    async def twitch_unsub(self, ctx: MyContext, streamer: str):
+        "Unsubscribe from a twitch user"
+        if streamer.isnumeric():
+            user_id = streamer
+        else:
+            if user := await self.agent.get_user_by_name(streamer):
+                user_id = user["id"]
+            else:
+                await ctx.send("User not found")
+                return
+        if await self.db_remove_streamer(ctx.guild.id, "twitch", user_id):
+            await ctx.send(f"Unsubscribed to channel")
+        else:
+            await ctx.send("User not subscribed!")
+
+    @twitch_unsub.autocomplete("streamer")
+    async def twitch_unsub_autocomplete(self, ctx: MyContext, current: str):
+        "Autocomplete for twitch_unsub"
+        current = current.lower()
+        streamers = await self.db_get_guild_streamers(ctx.guild.id, "twitch")
+        filtered = [
+            (not streamer["user_name"].lower().startswith(current), streamer["user_name"], streamer["user_id"])
+            for streamer in streamers
+            if current in streamer["user_name"].lower() or current == streamer["user_id"]
+        ]
+        filtered.sort()
+        return [
+            app_commands.Choice(name=name, value=value)
+            for _, name, value in filtered
+        ]
+
+    @twitch.command(name="list-subscriptions")
+    async def twitch_list(self, ctx: MyContext):
+        "List all subscribed twitch users"
+        streamers = await self.db_get_guild_streamers(ctx.guild.id, "twitch")
+        if streamers:
+            await ctx.send("Subscribed to:\n• " + "\n• ".join(streamer["user_name"] for streamer in streamers))
+
+    @twitch.command(name="check-stream")
+    async def test_twitch(self, ctx: MyContext, streamer: str):
+        "Test the Twitch API"
+        if streamer.isnumeric():
+            user_id = streamer
+        else:
+            user_id = (await self.agent.get_user_by_name(streamer))["id"]
+        resp = await self.agent.get_user_stream_by_id(user_id)
+        if len(resp) > 0:
+            stream = resp[0]
+            if stream["is_mature"] and not ctx.channel.is_nsfw():
+                await ctx.send("This stream is marked as mature, and this channel is not NSFW")
+                return
+            started_at = isoparse(stream["started_at"])
+            embed = discord.Embed(
+                title=f"{stream['user_name']} is streaming!",
+                url=f"https://twitch.tv/{stream['user_name']}",
+                color=0x6441A4,
+                timestamp=started_at,
+                description=stream["title"],
+            )
+            embed.set_image(url=stream["thumbnail_url"].format(width=1280, height=720))
+            await ctx.send(embed=embed)
+        else:
+            await ctx.send(f"User {streamer} is not streaming")
 
 
 async def setup(bot: Zbot):
--- libs/twitch/api_agent.py
+++ libs/twitch/api_agent.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from typing import Optional
 import aiohttp
 
-from libs.twitch.api_types import HttpTokenNotSet, TokenResponse
+from libs.twitch.types import HttpTokenNotSet, StreamObject, StreamerObject, TokenResponse
 
 
 class TwitchApiAgent:
@@ -57,13 +57,28 @@ class TwitchApiAgent:
             "Authorization": f"Bearer {self._token}"
         }            
 
-    async def get_user_stream(self, *user_name: str):
-        "Get the stream of a user"
+    async def get_user_by_name(self, username: str) -> Optional[StreamerObject]:
+        "Get the ID of a user from their username"
         if not self.is_token_valid:
             raise HttpTokenNotSet()
         if not self._session:
             self._session = aiohttp.ClientSession()
-        url = "https://api.twitch.tv/helix/streams?user_login=livetilla"
-        params = {"user_login": user_name}
+        url = "https://api.twitch.tv/helix/users"
+        params = {"login": username}
+        async with self._session.get(url, params=params, headers=await self._get_headers()) as resp:
+            data = await resp.json()
+            try:
+                return data["data"][0]
+            except IndexError:
+                return None
+
+    async def get_user_stream_by_id(self, *user_ids: str) -> list[StreamObject]:
+        "Get the stream of users specified by their IDs"
+        if not self.is_token_valid:
+            raise HttpTokenNotSet()
+        if not self._session:
+            self._session = aiohttp.ClientSession()
+        url = "https://api.twitch.tv/helix/streams"
+        params = {"user_id": user_ids}
         async with self._session.get(url, headers=await self._get_headers(), params=params) as resp:
-            return await resp.json()
+            return (await resp.json())["data"]
