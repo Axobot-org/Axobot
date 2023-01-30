e00019544ad8d1f7ac1473ab2c4e84e6f51751f5
ZRunner <z.runner.mc@gmail.com>
2023-01-30 14:06:45 -0500

    fix(roles-react): deleted custom emoji

--- fcts/roles_react.py
+++ fcts/roles_react.py
@@ -153,7 +153,7 @@ class RolesReact(commands.Cog):
     @rr_main.command(name="remove")
     @commands.check(checks.database_connected)
     @commands.check(checks.has_manage_guild)
-    async def rr_remove(self, ctx, emoji):
+    async def rr_remove(self, ctx: MyContext, emoji):
         """Remove a role react
 
         ..Example roles_react remove :uwu:
@@ -171,17 +171,17 @@ class RolesReact(commands.Cog):
             await self.rr_remove_role(l[0]['ID'])
         except Exception as err:
             self.bot.dispatch("command_error", ctx, err)
+            return
+        role = ctx.guild.get_role(l[0]['role'])
+        if role is None:
+            await ctx.send(await self.bot._(ctx.guild.id, "roles_react.rr-removed-2", e=old_emoji))
         else:
-            role = ctx.guild.get_role(l[0]['role'])
-            if role is None:
-                await ctx.send(await self.bot._(ctx.guild.id, "roles_react.rr-removed-2", e=old_emoji))
-            else:
-                await ctx.send(await self.bot._(ctx.guild.id, "roles_react.rr-removed", r=role, e=old_emoji))
-            if len(l) < 2:
-                try:
-                    self.guilds_which_have_roles.remove(ctx.guild.id)
-                except KeyError:
-                    pass
+            await ctx.send(await self.bot._(ctx.guild.id, "roles_react.rr-removed", r=role, e=old_emoji))
+        if len(l) < 2:
+            try:
+                self.guilds_which_have_roles.remove(ctx.guild.id)
+            except KeyError:
+                pass
 
     async def create_list_embed(self, liste: list[dict], guild: discord.Guild):
         """Create a text with the roles list"""
@@ -237,17 +237,22 @@ It will only display the whole message with reactions. Still very cool tho
             roles_list = await self.rr_list_role(ctx.guild.id)
         except Exception as err:
             self.bot.dispatch("command_error", ctx, err)
-        else:
-            des, emojis = await self.create_list_embed(roles_list, ctx.guild)
-            title = await self.bot._(ctx.guild.id, "roles_react.rr-embed")
-            emb = discord.Embed(title=title, description=des, color=self.embed_color, timestamp=ctx.message.created_at)
-            emb.set_footer(text=self.footer_txt)
-            msg = await ctx.send(embed=emb)
-            for err in emojis:
-                try:
-                    await msg.add_reaction(err)
-                except (discord.Forbidden, discord.NotFound):
-                    pass
+            return
+        des, emojis = await self.create_list_embed(roles_list, ctx.guild)
+        title = await self.bot._(ctx.guild.id, "roles_react.rr-embed")
+        emb = discord.Embed(title=title, description=des, color=self.embed_color, timestamp=ctx.message.created_at)
+        emb.set_footer(text=self.footer_txt)
+        msg = await ctx.send(embed=emb)
+        for emoji in emojis:
+            try:
+                await msg.add_reaction(emoji)
+            except (discord.Forbidden, discord.NotFound):
+                pass
+            except discord.HTTPException as err:
+                if err.status == 400:
+                    continue
+                self.bot.dispatch("command_error", ctx, err)
+                break
 
     @rr_main.command(name="join")
     @commands.check(checks.database_connected)
