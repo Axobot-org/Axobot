a6698217ddbc44e102b60a420fce4380465206e1
ZRunner <z.runner.mc@gmail.com>
2023-03-30 21:48:05 -0400

    feat(xp): new card generator

---
+++ libs/xp_cards/card_types.py
@@ -0,0 +1,36 @@
+from typing import Literal, TypedDict
+
+RgbColor = tuple[int, int, int]
+RgbaColor = tuple[int, int, int, int]
+
+
+class TextMetaData(TypedDict):
+    "Contains info about text position and size for a given card style"
+    position: tuple[tuple[int, int], tuple[int, int]] # (x1, y1), (x2, y2)
+    font_size: int
+    alignment: Literal["left", "center", "right"]
+    font: str
+
+class CardMetaData(TypedDict):
+    "Metadata about the card"
+    avatar_size: int
+    avatar_position: tuple[int, int]
+    texts: TextMetaData
+
+class ColorsData(TypedDict):
+    "Contains info about colors to apply to a card style"
+    xp_bar: RgbColor
+    texts: dict[str, RgbColor]
+
+class TextData(TextMetaData):
+    "Contains info about a text ready to be rendered"
+    color: RgbColor
+
+class CardData(TypedDict):
+    "Regroup every needed info to generate a card"
+    type: str
+    avatar_size: tuple[int, int]
+    avatar_position: tuple[int, int]
+    xp_bar_color: RgbColor
+    xp_percent: float
+    texts: dict[str, TextData]
