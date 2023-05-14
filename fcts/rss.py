8bf129eff2667599edf27a32646fa3a2a879a9c0
ZRunner <z.runner.mc@gmail.com>
2023-05-14 00:32:28 -0400

    fix(rss): typo in log

--- fcts/rss.py
+++ fcts/rss.py
@@ -1631,7 +1631,7 @@ class Rss(commands.Cog):
         if not self.bot.rss_enabled:
             return
         if not self.bot.database_online:
-            self.bot.log.warning('[rss ] Database is offline, skipping rss loop')
+            self.bot.log.warning('[rss] Database is offline, skipping rss loop')
             return
         self.bot.log.info(" Boucle rss commencée !")
         try:
