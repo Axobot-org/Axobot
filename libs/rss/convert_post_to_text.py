101f76d195f90bf0c44fb4269d8ee56e528d875b
ZRunner <z.runner.mc@gmail.com>
2023-07-13 15:16:14 -0400

    feat(rss): log url in error

--- libs/rss/convert_post_to_text.py
+++ libs/rss/convert_post_to_text.py
@@ -10,7 +10,7 @@ async def get_text_from_entry(entry: dict) -> Optional[str]:
         return await convert_post_to_text(entry["content"][0]["value"], entry["content"][0]["type"])
     if "summary" in entry:
         return await convert_post_to_text(entry["summary"], entry["summary_detail"]["type"])
-    log.error("[rss] No content or summary in entry")
+    log.error("[rss] No content or summary in entry for article %s", entry['link'])
     return None
 
 async def convert_post_to_text(post: str, post_type: str) -> Optional[str]:
