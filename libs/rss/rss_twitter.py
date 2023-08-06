00efbf12b8dab144e07b79abdc2a94827a96a420
ZRunner <z.runner.mc@gmail.com>
2023-08-06 14:59:55 -0400

    refact(rss): split feed parsing code for yt/tw/web

--- libs/rss/rss_twitter.py
+++ libs/rss/rss_twitter.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import datetime as dt
 import html
 import re
-from typing import TYPE_CHECKING, Optional, Union
+from typing import TYPE_CHECKING, Optional
 
 import discord
 import twitter
@@ -20,9 +20,9 @@ class TwitterRSS:
 
     def __init__(self, bot: Axobot):
         self.bot = bot
-        self.min_time_between_posts = 15
+        self.min_time_between_posts = 15 # seconds
         self.api = twitter.Api(**bot.others['twitter'], tweet_mode="extended", timeout=15, application_only_auth=True)
-        self.url_pattern = r'(?:http.*://)?(?:www\.)?(?:twitter\.com/)([^?\s/]+)'
+        self.url_pattern = r'(?:https://)?(?:www\.)?(?:twitter\.com/)([^?\s/]+)'
 
     def is_twitter_url(self, string: str):
         "Check if an url is a valid Twitter URL"
@@ -64,81 +64,100 @@ class TwitterRSS:
                     text = text.replace(match.group(0), '')
         return text
 
-    async def get_feed(self, channel: discord.TextChannel, name: str, date: dt.datetime=None) -> Union[str, list[RssMessage]]:
+    async def _get_feed_list(self,name: str):
         "Get tweets from a given Twitter user"
         try:
             if isinstance(name, int) or name.isnumeric():
                 posts = self.api.GetUserTimeline(user_id=int(name), exclude_replies=True)
-                username = (await self.get_user_from_id(int(name))).screen_name
+                # username = (await self.get_user_from_id(int(name))).screen_name
             else:
                 posts = self.api.GetUserTimeline(screen_name=name, exclude_replies=True)
-                username = name
+                # username = name
         except twitter.error.TwitterError as err:
             if err.message == "Not authorized." or 'Unknown error' in err.message:
-                return await self.bot._(channel, "rss.nothing")
+                return None
             if err.message[0]['code'] == 34:
-                return await self.bot._(channel, "rss.nothing")
+                return None
             raise err
-        if not date:
-            if len(posts) == 0:
-                return []
-            lastpost = posts[0]
+        return posts
+
+    async def get_last_post(self, channel: discord.TextChannel, name: str) -> Optional[RssMessage]:
+        "Get the last post from a given Twitter user"
+        # fetch tweets
+        posts = await self._get_feed_list(name)
+        if len(posts) == 0:
+            return await self.bot._(channel, "rss.nothing")
+        # get username
+        if isinstance(name, int) or name.isnumeric():
+            username = (await self.get_user_from_id(int(name))).screen_name
+        else:
+            username = name
+        lastpost = posts[0]
+        # detect if retweet
+        is_rt = None
+        text = html.unescape(getattr(lastpost, 'full_text', lastpost.text))
+        if lastpost.retweeted:
+            if possible_rt := re.search(r'^RT @([\w-]+):', text):
+                is_rt = possible_rt.group(1)
+        # remove images links if needed
+        text = await self.remove_image(channel, text)
+        # format URL
+        url = f"https://twitter.com/{username.lower()}/status/{lastpost.id}"
+        img = None
+        if lastpost.media: # if exists and is not empty
+            img = lastpost.media[0].media_url_https
+        return RssMessage(
+            bot=self.bot,
+            feed=FeedObject.unrecorded("tw", channel.guild.id if channel.guild else None, channel.id, url),
+            url=url,
+            title=text,
+            date=dt.datetime.fromtimestamp(lastpost.created_at_in_seconds),
+            author=lastpost.user.screen_name,
+            retweeted_from=is_rt,
+            channel=lastpost.user.name,
+            image=img,
+            post_text=getattr(lastpost, 'full_text', lastpost.text)
+        )
+
+    async def get_new_posts(self, channel: discord.TextChannel, name: str, date: dt.datetime) -> list[RssMessage]:
+        "Get new posts from a given Twitter user"
+        # fetch tweets
+        posts = await self._get_feed_list(name)
+        if len(posts) == 0:
+            return []
+        posts_list = []
+        for post in posts:
+            # don't return more than 10 posts
+            if len(posts_list) > 10:
+                break
+            # don't return posts older than the date
+            if (dt.datetime.fromtimestamp(post.created_at_in_seconds) - date).total_seconds() < self.min_time_between_posts:
+                break
+            # detect if retweet
             is_rt = None
-            text = html.unescape(getattr(lastpost, 'full_text', lastpost.text))
-            if lastpost.retweeted:
+            text: str = html.unescape(getattr(post, 'full_text', post.text))
+            if post.retweeted:
                 if possible_rt := re.search(r'^RT @([\w-]+):', text):
                     is_rt = possible_rt.group(1)
             # remove images links if needed
             text = await self.remove_image(channel, text)
             # format URL
-            url = f"https://twitter.com/{username.lower()}/status/{lastpost.id}"
+            url = f"https://twitter.com/{name.lower()}/status/{post.id}"
             img = None
-            if lastpost.media: # if exists and is not empty
-                img = lastpost.media[0].media_url_https
+            if post.media: # if exists and is not empty
+                img = post.media[0].media_url_https
             obj = RssMessage(
                 bot=self.bot,
                 feed=FeedObject.unrecorded("tw", channel.guild.id if channel.guild else None, channel.id, url),
                 url=url,
                 title=text,
-                date=dt.datetime.fromtimestamp(lastpost.created_at_in_seconds),
-                author=lastpost.user.screen_name,
+                date=dt.datetime.fromtimestamp(post.created_at_in_seconds),
+                author=post.user.screen_name,
                 retweeted_from=is_rt,
-                channel=lastpost.user.name,
+                channel=post.user.name,
                 image=img,
-                post_text=getattr(lastpost, 'full_text', lastpost.text)
+                post_text=getattr(post, 'full_text', post.text)
             )
-            return [obj]
-        else:
-            liste = []
-            for post in posts:
-                if len(liste)>10:
-                    break
-                if (dt.datetime.fromtimestamp(post.created_at_in_seconds) - date).total_seconds() < self.min_time_between_posts:
-                    break
-                is_rt = None
-                text: str = html.unescape(getattr(post, 'full_text', post.text))
-                if post.retweeted:
-                    if possible_rt := re.search(r'^RT @([\w-]+):', text):
-                        is_rt = possible_rt.group(1)
-                # remove images links if needed
-                text = await self.remove_image(channel, text)
-                # format URL
-                url = f"https://twitter.com/{name.lower()}/status/{post.id}"
-                img = None
-                if post.media: # if exists and is not empty
-                    img = post.media[0].media_url_https
-                obj = RssMessage(
-                    bot=self.bot,
-                    feed=FeedObject.unrecorded("tw", channel.guild.id if channel.guild else None, channel.id, url),
-                    url=url,
-                    title=text,
-                    date=dt.datetime.fromtimestamp(post.created_at_in_seconds),
-                    author=post.user.screen_name,
-                    retweeted_from=is_rt,
-                    channel=post.user.name,
-                    image=img,
-                    post_text=getattr(lastpost, 'full_text', lastpost.text)
-                )
-                liste.append(obj)
-            liste.reverse()
-            return liste
+            posts_list.append(obj)
+        posts_list.reverse()
+        return posts_list
