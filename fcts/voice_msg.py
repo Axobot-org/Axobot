7305b58b8f57540fe24ba38efe10c3cbf791f106
ZRunner <z.runner.mc@gmail.com>
2023-09-23 11:57:56 -0400

    refact(tokens): edit whisper API token key

--- fcts/voice_msg.py
+++ fcts/voice_msg.py
@@ -88,7 +88,7 @@ class VoiceMessages(commands.Cog):
 
     async def _get_transcript(self, attachment: discord.Attachment) -> str:
         "Call the external API to get the audio transcript"
-        headers = {'Authorization': self.bot.others["awhikax-api"]}
+        headers = {'Authorization': self.bot.others["awhikax_api"]}
         data = {'status': "default", 'audio': attachment.url}
         async with self.session.post("https://api.awhikax.com/stt", headers=headers, data=data) as resp:
             if resp.status != 200:
