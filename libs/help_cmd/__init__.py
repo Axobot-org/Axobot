6f027d4b06f9850c98fbd5efa968cbbdc344eaca
ZRunner <z.runner.mc@gmail.com>
2023-11-24 18:10:58 -0500

    feat(help): add slash command support

--- libs/help_cmd/__init__.py
+++ libs/help_cmd/__init__.py
@@ -1,9 +1,11 @@
 from .help_all import help_all_command
 from .help_category import help_category_command
 from .help_cmd import help_text_cmd_command
+from .help_slash import help_slash_cmd_command
 
 __all__ = (
     "help_all_command",
     "help_category_command",
     "help_text_cmd_command",
+    "help_slash_cmd_command",
 )
