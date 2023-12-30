79f92ab95e70fb1b67e80d330aeeaab3bf650fd9
ZRunner <z.runner.mc@gmail.com>
2023-12-30 17:24:32 +0100

    refact(roles-react): start to cleanup file code

--- libs/getch_methods.py
+++ libs/getch_methods.py
@@ -9,3 +9,12 @@ async def getch_channel_or_thread(guild: discord.Guild, channel_id: int):
         return await guild.fetch_channel(channel_id)
     except discord.NotFound:
         return None
+
+async def getch_member(guild: discord.Guild, member_id: int):
+    "Get a member from its id, or None if not found"
+    if member := guild.get_member(member_id):
+        return member
+    try:
+        return await guild.fetch_member(member_id)
+    except discord.NotFound:
+        return None
