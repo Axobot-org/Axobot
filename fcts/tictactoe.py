776ede3ba0d1270124901ff23ca36f816affd5b4
ZRunner <z.runner.mc@gmail.com>
2024-04-08 11:20:26 -0400

    refact(ttt): use discord buttons instead of text input

--- fcts/tictactoe.py
+++ fcts/tictactoe.py
@@ -1,4 +1,3 @@
-import asyncio
 import random
 import time
 from typing import Literal, Optional, Union
@@ -23,12 +22,6 @@ class TicTacToe(commands.Cog):
         self.in_game = {}
         self.types: tuple[str] = options['ttt_display']['values']
 
-    async def get_ttt_mode(self, interaction: discord.Interaction) -> str:
-        """Get the used mode for a specific context"""
-        if interaction.guild is None:
-            return options["ttt_display"]["default"]
-        return await self.bot.get_config(interaction.guild_id, "ttt_display")
-
     @app_commands.command(name="tic-tac-toe")
     @app_commands.check(is_ttt_enabled)
     async def main(self, interaction: discord.Interaction):
@@ -42,23 +35,25 @@ class TicTacToe(commands.Cog):
             return
         await interaction.response.defer()
         self.in_game[interaction.user.id] = time.time()
-        game = self.Game(interaction, self, await self.get_ttt_mode(interaction))
-        await game.init_emojis()
-        await game.start()
+        game = self.Game(interaction, self)
+        await game.init_game()
+        u_begin = await self.bot._(interaction, "tictactoe.user-begin" if game.is_user_turn else "tictactoe.bot-begin")
+        tip = await self.bot._(interaction, "tictactoe.tip", symb1=game.emojis[0], symb2=game.emojis[1])
+        await interaction.edit_original_response(content=u_begin.format(interaction.user.mention) + tip, view=game)
+        await game.wait()
         self.in_game.pop(interaction.user.id, None)
 
-    class Game():
+    class Game(discord.ui.View):
         "An actual tictactoe game running"
 
-        def __init__(self, interaction: discord.Interaction, cog: "TicTacToe", mode: Literal["disabled", "short", "normal"]):
+        def __init__(self, interaction: discord.Interaction, cog: "TicTacToe"):
+            super().__init__(timeout=60)
             self.cog = cog
             self.interaction = interaction
             self.bot = cog.bot
-            self.use_short = mode == "short"
-            self.grid: GridType = list(range(1, 10))
+            self.grid: GridType = list(range(0, 9))
             self.is_user_turn = random.random() < 0.5
             self.emojis: tuple[str, str] = tuple()
-            self.valid_inputs = [str(x) for x in range(1, 10)]
             self.winning_combinations = [
                 [0, 1, 2],
                 [3, 4, 5],
@@ -70,7 +65,7 @@ class TicTacToe(commands.Cog):
                 [2, 4, 6]
             ]
 
-        async def init_emojis(self):
+        async def init_game(self):
             "Init the emojis used to play"
             if self.bot.current_event == 'halloween':
                 self.emojis = ("🎃", ":bat:")
@@ -82,37 +77,42 @@ class TicTacToe(commands.Cog):
                 self.emojis = await self.bot.get_config(self.interaction.guild_id, "ttt_emojis")
             if len(self.emojis) < 2:
                 self.emojis = (':red_circle:', ':blue_circle:')
-
-        async def display_grid(self) -> str:
-            """Generate the grid to display in the chat"""
-            display_grid = ''
-            if self.interaction.channel.permissions_for(self.interaction.guild.me).use_external_emojis:
-                emojis = [f"<:{x}>" for x in self.bot.emojis_manager.numbers_names]
+            # if the bot should start, play its turn
+            if not self.is_user_turn:
+                await self.bot_turn()
             else:
-                emojis = [chr(48+i)+chr(8419) for i in range(10)]
-            for k in range(9):
-                if k % 3 == 0:
-                    display_grid += '\n'
-                if self.grid[k] in range(10):
-                    display_grid += emojis[self.grid[k]]
-                elif self.grid[k] == 'O':
-                    display_grid += self.emojis[0]
+                await self.update_grid()
+
+        async def update_grid(self, content: Optional[str] = discord.utils.MISSING):
+            """Update the view buttons with the current grid state"""
+            self.clear_items()
+            for i in range(9):
+                row = i // 3
+                if self.grid[i] in range(10):
+                    button = discord.ui.Button(
+                        style=discord.ButtonStyle.blurple, label=str(i+1), custom_id=f"ttt_{i}", row=row,
+                        disabled=self.is_finished()
+                    )
+                    button.callback = self.on_click
+                elif self.grid[i] == 'O':
+                    button = discord.ui.Button(style=discord.ButtonStyle.gray, label=self.emojis[0], disabled=True, row=row)
                 else:
-                    display_grid += self.emojis[1]
-            return display_grid
+                    button = discord.ui.Button(style=discord.ButtonStyle.gray, label=self.emojis[1], disabled=True, row=row)
+                self.add_item(button)
+            await self.interaction.edit_original_response(content=content, view=self)
 
-        async def test_valid_cell(self, user_input: str):
+        async def test_valid_cell(self, case_id: int):
             """Check if the cell is empty"""
-            return str(self.grid[int(user_input)-1]) not in {'X', 'O'}
+            return self.grid[case_id] not in {'X', 'O'}
 
-        async def replace_cell(self, is_user_turn: bool, user_input: str):
-            """Replace the cell value in the grid with the player's symbol"""
+        async def replace_cell(self, is_user_turn: bool, case_id: int):
+            "Replace the cell value in the grid with the player's symbol, and return a copy of the grid"
             if is_user_turn:
-                return ['X' if x == int(user_input) else x for x in self.grid]
-            return ['O' if x == int(user_input) else x for x in self.grid]
+                return ['X' if x == case_id else x for x in self.grid]
+            return ['O' if x == case_id else x for x in self.grid]
 
-        async def test_win(self, grid: GridType):
-            """Test s'il y a une position de victoire"""
+        async def test_victory(self, grid: GridType):
+            "Test if one player has won the game"
             for winning_combination in self.winning_combinations:
                 if grid[winning_combination[0]] == grid[winning_combination[1]] == grid[winning_combination[2]]:
                     return True
@@ -126,96 +126,61 @@ class TicTacToe(commands.Cog):
             "Test if the whole grid is empty"
             return all(isinstance(x, int) for x in self.grid)
 
-        async def _send_initial_message(self):
-            u_begin = await self.bot._(self.interaction, "tictactoe.user-begin" if self.is_user_turn else "tictactoe.bot-begin")
-            tip = await self.bot._(self.interaction, "tictactoe.tip", symb1=self.emojis[0], symb2=self.emojis[1])
-            await self.interaction.followup.send(u_begin.format(self.interaction.user.mention) + tip)
+        async def on_click(self, interaction: discord.Interaction):
+            "Handle the user's click on the grid"
+            if interaction.user.id != self.interaction.user.id or not self.is_user_turn:
+                return
+            await interaction.response.defer()
+            case_id = int(interaction.data["custom_id"].split("_")[1])
+            if not await self.test_valid_cell(case_id):
+                self.bot.dispatch("error", ValueError("Invalid cell"), "During a tictactoe game")
+                return
+            self.grid = await self.replace_cell(self.is_user_turn, case_id)
+            self.interaction = interaction
+            if await self.check_game_end():
+                return
+            self.is_user_turn = False
+            await self.bot_turn()
 
-        async def start(self):
-            "Actually starts the game"
-            await self._send_initial_message()
-            send_msg = self.interaction.followup.send
-            is_draw = True
+        async def bot_turn(self):
+            "Make the bot play its turn"
+            # Prepare the fallback answer as a random empty cell
+            case_id = random.choice([i for i, x in enumerate(self.grid) if isinstance(x, int)])
+            # Check if the user is about to win, or if the bot can win
+            for k in range(0, 9):
+                if await self.test_valid_cell(k):
+                    for i in [True, False]:
+                        grid_copy = await self.replace_cell(i, k)
+                        if await self.test_victory(grid_copy):
+                            case_id = k
+                            break
+            # Update the game state
+            self.grid = await self.replace_cell(self.is_user_turn, case_id)
+            if await self.check_game_end():
+                return
+            self.is_user_turn = True
+            # Edit the message
+            await self.update_grid()
 
-            def check(msg: discord.Message):
-                return msg.channel == self.interaction.channel and msg.author == self.interaction.user
-
-            display_grid = True
-            last_grid: Optional[discord.WebhookMessage] = None
-            while await self.test_any_empty_cell():
-                if self.interaction.user.id not in self.cog.in_game.keys():
-                    return
-
-            ###
-                if self.is_user_turn:
-                    if display_grid:
-                        # if needed, clean the messages
-                        if self.use_short and last_grid:
-                            await last_grid.delete()
-                        last_grid = await send_msg(await self.display_grid(), wait=True)
-                    display_grid = True
-                    try:
-                        msg: discord.Message = await self.bot.wait_for("message", check=check, timeout=50)
-                    except asyncio.TimeoutError:
-                        await send_msg(await self.bot._(self.interaction, "tictactoe.too-late"))
-                        return
-                    user_input = msg.content
-                    if msg.content in self.valid_inputs:
-                        if await self.test_valid_cell(user_input):
-                            self.grid = await self.replace_cell(self.is_user_turn, user_input)
-                            self.is_user_turn = False
-                            if self.use_short:
-                                await msg.delete(delay=0.1)
-                        else: # cell is not empty
-                            await send_msg(await self.bot._(self.interaction, "tictactoe.pion-1"))
-                            display_grid = False
-                            continue
-                    elif msg.content.endswith("leave"): # user leaves the game
-                        return
-                    else: # invalid cell number
-                        await send_msg(await self.bot._(self.interaction, "tictactoe.pion-2"))
-                        display_grid = False
-                        continue
-            ###
-                else:  # Bot's turn
-                    # Prepare the fallback answer as a random cell
-                    user_input = random.randint(1, 10)
-                    # Check if the user is about to win, or if the bot can win
-                    for k in range(1, 10):
-                        for i in [True, False]:
-                            grid_copy = await self.replace_cell(i, k)
-                            if await self.test_win(grid_copy):
-                                user_input = k
-                                break
-                    # Check if value is valid
-                    if str(user_input) in self.valid_inputs:
-                        if await self.test_valid_cell(user_input):
-                            self.grid = await self.replace_cell(self.is_user_turn, user_input)
-                            self.is_user_turn = True
-                        else:
-                            continue
-                    else:
-                        continue
-                    display_grid = True
-            ###
-                if await self.test_win(self.grid):
-                    is_draw = False
-                    break
-            ###
-            # if needed, clean the messages
-            if self.use_short and last_grid:
-                await last_grid.delete()
-            if is_draw:
-                game_result = await self.bot._(self.interaction, "tictactoe.nul")
+        async def check_game_end(self):
+            "Check if anyone won the game, or if no cell is empty"
+            if await self.test_victory(self.grid): # someone won
+                is_draw = False
+                if self.is_user_turn: # The user won
+                    result = await self.bot._(self.interaction, "tictactoe.win-user", user=self.interaction.user.mention)
+                else: # The bot won
+                    result = await self.bot._(self.interaction, "tictactoe.win-bot")
+            elif not await self.test_any_empty_cell(): # No empty cell
+                is_draw = True
+                result = await self.bot._(self.interaction, "tictactoe.nul")
             else:
-                if self.is_user_turn:  # The bot won
-                    game_result = await self.bot._(self.interaction, "tictactoe.win-bot")
-                else:  # The user won
-                    game_result = await self.bot._(self.interaction, "tictactoe.win-user", user=self.interaction.user.mention)
-            await send_msg(await self.display_grid() + '\n' + game_result)
-            if not is_draw and not self.is_user_turn:
+                return False
+            self.stop()
+            await self.update_grid(result)
+            if not is_draw and self.is_user_turn:
                 # give event points if user won
                 await self.cog.give_event_points(self.interaction.channel, self.interaction.user, 8)
+            return True
 
     async def give_event_points(self, channel: "discord.interactions.InteractionChannel", user: discord.User, points: int):
         "Give points to a user and check if they had unlocked a card"
