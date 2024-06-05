4be113de386b430f4cee7cd2b7c1954c4a37bd9e
ZRunner <z.runner.mc@gmail.com>
2024-06-05 15:42:37 +0200

    feat(slash): convert bitly

--- modules/bitly/bitly.py
+++ modules/bitly/bitly.py
@@ -1,9 +1,10 @@
+import discord
 from discord import app_commands
 from discord.ext import commands
 
-from modules.bitly import bitly_api
 from core.arguments import args
-from core.bot_classes import Axobot, MyContext
+from core.bot_classes import Axobot
+from modules.bitly import bitly_api
 
 
 class Bitly(commands.Cog):
@@ -14,51 +15,40 @@ class Bitly(commands.Cog):
         self.file = "bitly"
         self.bitly_client = bitly_api.Bitly(api_key=self.bot.others['bitly'])
 
-    @commands.hybrid_group(name="bitly")
-    async def bitly_main(self, ctx: MyContext):
-        """Bit.ly website, but in Discord
-        Create shortened url and unpack them by using Bitly services
-
-        ..Doc miscellaneous.html#bitly-urls"""
-        if ctx.subcommand_passed is None:
-            await ctx.send_help(ctx.command)
-        elif ctx.invoked_subcommand is None and ctx.subcommand_passed is not None:
-            try:
-                url = await args.URL.convert(ctx,ctx.subcommand_passed)
-            except commands.BadArgument:
-                return
-            if url.domain in ['bit.ly','bitly.com','bitly.is']:
-                await self.bitly_find(ctx, url)
-            else:
-                await self.bitly_create(ctx, url)
+    bitly_main = app_commands.Group(
+        name="bitly",
+        description="Create shortened url and unpack them by using Bitly services",
+    )
 
     @bitly_main.command(name="create")
     @app_commands.describe(url="The url you want to shorten")
     @app_commands.checks.cooldown(3, 15)
-    async def bitly_create(self, ctx: MyContext, url: args.URL):
+    async def bitly_create(self, interaction: discord.Interaction, url: args.URLArgument):
         """Create a shortened url
 
         ..Example bitly create https://fr-minecraft.net
 
         ..Doc miscellaneous.html#bitly-urls"""
         if url.domain == 'bit.ly':
-            return await ctx.send(await self.bot._(ctx.channel,'info.bitly_already_shortened'))
-        await ctx.defer()
-        await ctx.send(await self.bot._(ctx.channel,'info.bitly_short', url=self.bitly_client.shorten_url(url.url)))
+            return await interaction.response.send_message(await self.bot._(interaction, "info.bitly_already_shortened"))
+        await interaction.response.defer()
+        shortened_url = self.bitly_client.shorten_url(url.url)
+        await interaction.followup.send(await self.bot._(interaction, "info.bitly_short", url=shortened_url))
 
     @bitly_main.command(name="find")
     @app_commands.describe(url="The url to expand. Should starts with https://bit.ly/")
     @app_commands.checks.cooldown(3, 15)
-    async def bitly_find(self, ctx: MyContext, url: args.URL):
+    async def bitly_find(self, interaction: discord.Interaction, url: args.URLArgument):
         """Find the long url from a bitly link
 
         ..Example bitly find https://bit.ly/2JEHsUf
 
         ..Doc miscellaneous.html#bitly-urls"""
         if url.domain != 'bit.ly':
-            return await ctx.send(await self.bot._(ctx.channel,'info.bitly_nobit'))
-        await ctx.defer()
-        await ctx.send(await self.bot._(ctx.channel,'info.bitly_long', url=self.bitly_client.expand_url(url.url)))
+            return await interaction.response.send_message(await self.bot._(interaction, "info.bitly_nobit"))
+        await interaction.response.defer()
+        expanded_url = self.bitly_client.expand_url(url.url)
+        await interaction.followup.send(await self.bot._(interaction, "info.bitly_long", url=expanded_url))
 
 
 async def setup(bot):
