48a282b6fbcc482b326589b3a0a7ede83b4e12f9
ZRunner <z.runner.mc@gmail.com>
2024-06-11 13:29:21 +0200

    feat(slash): convert permissions cmd

--- modules/perms/perms.py
+++ modules/perms/perms.py
@@ -34,29 +34,32 @@ AcceptableTargetTypes = (
     | RawPermissionValue
 )
 
-
-class TargetConverter(commands.Converter):
+class TargetTransformer(app_commands.Transformer): # pylint: disable=abstract-method
     "Convert a string argument into a Discord Member, Role or Permission object"
-    async def convert(self, ctx: MyContext, argument: str) -> AcceptableTargetTypes:
+    async def transform(self, interaction, value, /):
+        ctx = await MyContext.from_interaction(interaction)
         try:
-            return await commands.MemberConverter().convert(ctx, argument)
+            return await commands.MemberConverter().convert(ctx, value)
         except commands.MemberNotFound:
             pass
 
         try:
-            return await commands.RoleConverter().convert(ctx, argument)
+            return await commands.RoleConverter().convert(ctx, value)
         except commands.RoleNotFound:
             pass
 
         try:
-            return await RawPermissionValue().convert(ctx, argument)
+            return await RawPermissionValue().convert(ctx, value)
         except commands.BadArgument:
             pass
 
-        if argument == "everyone":
+        if value == "everyone":
             return ctx.guild.default_role
 
-        raise InvalidPermissionTargetError(argument)
+        raise InvalidPermissionTargetError(value)
+
+TargetArgument = app_commands.Transform[AcceptableTargetTypes, TargetTransformer]
+
 
 class Perms(commands.Cog):
     """Cog with a single command, allowing you to see the permissions of a member or a role in a channel."""
@@ -65,18 +68,21 @@ class Perms(commands.Cog):
         self.bot = bot
         self.file = "perms"
         chan_perms = [key for key,value in discord.Permissions().all_channel() if value]
-        self.perms_name = {'general':[key for key,value in discord.Permissions().general() if value],
-            'text':[key for key,value in discord.Permissions().text() if value],
-            'voice':[key for key,value in discord.Permissions().voice() if value]}
+        self.perms_name = {
+            'general': [key for key,value in discord.Permissions().general() if value],
+            'text': [key for key,value in discord.Permissions().text() if value],
+            'voice': [key for key,value in discord.Permissions().voice() if value]
+        }
         self.perms_name['common_channel'] = [x for x in chan_perms if x in self.perms_name['general']]
 
-    async def collect_permissions(self, ctx: MyContext, permissions: discord.Permissions, channel) -> list[tuple[str, str]]:
+    async def collect_permissions(self, interaction: discord.Interaction, permissions: discord.Permissions, channel
+                                  ) -> list[tuple[str, str]]:
         "Iterate over the given permissions and return the needed ones, formatted"
         result = []
         emojis_cog = self.bot.emojis_manager
         # if target is admin, only display that
         if permissions.administrator:
-            perm_tr = await self.bot._(ctx.guild.id, "permissions.list.administrator")
+            perm_tr = await self.bot._(interaction, "permissions.list.administrator")
             if "permissions.list." in perm_tr: # unsuccessful translation
                 perm_tr = "Administrator"
             return [(emojis_cog.customs['green_check'], perm_tr)]
@@ -93,7 +99,7 @@ class Perms(commands.Cog):
                 (perm_id in voice_perms and isinstance(channel, typing.get_args(VoiceChannelTypes)))
             ):
                 continue
-            perm_tr = await self.bot._(ctx.guild.id, "permissions.list."+perm_id)
+            perm_tr = await self.bot._(interaction, "permissions.list."+perm_id)
             if "permissions.list." in perm_tr:  # unsuccessful translation
                 perm_tr = perm_id.replace('_', ' ').title()
             if value:
@@ -102,18 +108,18 @@ class Perms(commands.Cog):
                 result.append((emojis_cog.customs['red_cross'], perm_tr))
         return result
 
-    @commands.hybrid_command(name='permissions', aliases=['perms'])
+    @app_commands.command(name='permissions')
+    @app_commands.guild_only()
     @app_commands.default_permissions(manage_roles=True)
     @app_commands.describe(
         channel="The channel to check the permissions in",
         target="The member or role to check the permissions of, or an integer/binary value"
     )
-    @commands.guild_only()
-    async def check_permissions(self, ctx: MyContext, channel: AcceptableChannelTypes=None, *,
-                                target: typing.Annotated[AcceptableTargetTypes, TargetConverter]=None):
+    async def check_permissions(self, interaction: discord.Interaction, channel: AcceptableChannelTypes = None,
+                                target: TargetArgument = None):
         """Check the permissions assigned to a member/role
         By default, it will calculate the author's permissions at the server level.
-        You can also choose to view the permissions associated to a raw integer/binary value (in which case channel will be ignored)
+        You can also choose to view the permissions associated to a raw integer/binary value (if so, 'channel' will be ignored)
 
         ..Example permissions #announcements everyone
 
@@ -123,7 +129,8 @@ class Perms(commands.Cog):
 
         ..Doc infos.html#permissions"""
         if target is None:
-            target = ctx.author
+            target = interaction.user
+        await interaction.response.defer()
         if isinstance(target, discord.Member):
             if channel is None:
                 perms = target.guild_permissions
@@ -131,47 +138,46 @@ class Perms(commands.Cog):
                 perms = channel.permissions_for(target)
             col = target.color
             avatar = target.display_avatar.replace(static_format="png", size=256)
-            name = await self.bot._(ctx, "permissions.target.member", name=target.display_name)
+            name = await self.bot._(interaction, "permissions.target.member", name=target.display_name)
         elif isinstance(target, discord.Role):
             if channel is None:
                 perms = target.permissions
             else:
                 perms = channel.permissions_for(target)
             col = target.color
-            avatar = ctx.guild.icon.replace(format='png', size=256) if ctx.guild.icon else None
-            name = await self.bot._(ctx, "permissions.target.role", name=str(target))
+            avatar = interaction.guild.icon.replace(format='png', size=256) if interaction.guild.icon else None
+            name = await self.bot._(interaction, "permissions.target.role", name=str(target))
         elif isinstance(target, int):
             perms = discord.Permissions(target)
             col = discord.Color.blurple()
             avatar = None
-            name = await self.bot._(ctx, "permissions.target.value", value=f"{target} | {bin(target)}")
+            name = await self.bot._(interaction, "permissions.target.value", value=f"{target} | {bin(target)}")
         else:
-            self.bot.dispatch("error", TypeError(f"Unknown target type: {type(target)}"), ctx)
+            self.bot.dispatch("error", TypeError(f"Unknown target type: {type(target)}"), interaction)
             return
 
-        perms_list = await self.collect_permissions(ctx, perms, channel)
+        perms_list = await self.collect_permissions(interaction, perms, channel)
         perms_list.sort(key=lambda x: x[1])
         perms_list = [''.join(perm) for perm in perms_list]
         if isinstance(target, int):
             desc = None
         elif channel is None:
-            desc = await self.bot._(ctx, "permissions.channel.general")
+            desc = await self.bot._(interaction, "permissions.channel.general")
         elif isinstance(channel, discord.CategoryChannel):
-            desc = await self.bot._(ctx, "permissions.channel.category", name=channel.name)
+            desc = await self.bot._(interaction, "permissions.channel.category", name=channel.name)
         else:
-            desc = await self.bot._(ctx, "permissions.channel.channel", mention=channel.mention)
+            desc = await self.bot._(interaction, "permissions.channel.channel", mention=channel.mention)
 
         embed = discord.Embed(color=col, description=desc)
         paragraphs = cut_text(perms_list, max_size=21)
         for paragraph in paragraphs:
             embed.add_field(name=self.bot.zws, value=paragraph)
 
-        _whatisthat = await self.bot._(ctx, "permissions.whatisthat")
+        _whatisthat = await self.bot._(interaction, "permissions.whatisthat")
         embed.add_field(name=self.bot.zws, value=f'[{_whatisthat}](https://axobot.readthedocs.io/en/latest/perms.html)',
                         inline=False)
         embed.set_author(name=name, icon_url=avatar)
-        embed.set_footer(text=ctx.author, icon_url=ctx.author.display_avatar)
-        await ctx.send(embed=embed)
+        await interaction.followup.send(embed=embed)
 
 
 async def setup(bot):
