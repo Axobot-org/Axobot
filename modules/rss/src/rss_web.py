38455f0a3a8d727bf9184e0b344103f53c103cd1
ZRunner <z.runner.mc@gmail.com>
2024-07-08 11:22:26 +0200

    refact(rss): factorize feed entry parsing methods

--- modules/rss/src/rss_web.py
+++ modules/rss/src/rss_web.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import datetime as dt
 import re
 import time
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Any, Literal
 
 import aiohttp
 import discord
@@ -63,25 +63,14 @@ class WebRSS:
             if entry.get(i) is not None:
                 return i
 
-    async def _get_entry_title(self, entry: FeedParserDict) -> str | None:
+    async def _get_entry_id(self, entry: FeedParserDict) -> str | None:
         "Try to find the article ID or title"
         for i in ["id", "title", "updated_parsed"]:
             if value := entry.get(i):
                 return value
 
-    async def get_last_post(self, channel: discord.TextChannel, url: str,
-                            filter_config: FeedFilterConfig | None,
-                            session: aiohttp.ClientSession | None=None):
-        "Get the last post from a web feed"
-        feed = await self._get_feed(url, filter_config, session)
-        if not feed:
-            return await self.bot._(channel, "rss.web-invalid")
-        entry = feed.entries[0]
-        date_field_key = await self._get_feed_date_key(entry)
-        if date_field_key is None:
-            date = "Unknown"
-        else:
-            date = entry[date_field_key]
+    async def _parse_entry(self, entry: FeedParserDict, feed: FeedParserDict, url: str, date: Any, channel: discord.TextChannel):
+        "Parse a feed entry to get the relevant information and return a RssMessage object"
         if "link" in entry:
             link = entry["link"]
         elif "link" in feed:
@@ -114,13 +103,28 @@ class WebRSS:
             url=link,
             title=title,
             date=date,
-            entry_id=await self._get_entry_title(entry),
+            entry_id=await self._get_entry_id(entry),
             author=author,
             channel=feed.feed["title"] if "title" in feed.feed else '?',
             image=img,
             post_text=post_text,
             post_description=post_description
         )
+
+    async def get_last_post(self, channel: discord.TextChannel, url: str,
+                            filter_config: FeedFilterConfig | None,
+                            session: aiohttp.ClientSession | None=None):
+        "Get the last post from a web feed"
+        feed = await self._get_feed(url, filter_config, session)
+        if not feed:
+            return await self.bot._(channel, "rss.web-invalid")
+        entry = feed.entries[0]
+        date_field_key = await self._get_feed_date_key(entry)
+        if date_field_key is None:
+            entry_date = "Unknown"
+        else:
+            entry_date = entry[date_field_key]
+        return await self._parse_entry(entry, feed, url, entry_date, channel)
 
     async def get_new_posts(self, channel: discord.TextChannel, url: str, date: dt.datetime,
                             filter_config: FeedFilterConfig | None,
@@ -149,49 +153,11 @@ class WebRSS:
             if entry_date is None or (entry_date - date).total_seconds() < self.min_time_between_posts:
                 # we know we can break because entries are sorted by most recent first
                 break
-            entry_id = await self._get_entry_title(entry)
+            entry_id = await self._get_entry_id(entry)
             if last_entry_id is not None:
                 if entry_id == last_entry_id:
                     continue
-            if "link" in entry:
-                link = entry["link"]
-            elif "link" in feed:
-                link = feed["link"]
-            else:
-                link = url
-            if "author" in entry:
-                author = entry["author"]
-            elif "author" in feed:
-                author = feed["author"]
-            elif "title" in feed["feed"]:
-                author = feed["feed"]["title"]
-            else:
-                author = '?'
-            if "title" in entry:
-                title = entry["title"]
-            elif "title" in feed:
-                title = feed["title"]
-            else:
-                title = '?'
-            post_text = await get_text_from_entry(entry)
-            post_description = await get_summary_from_entry(entry)
-            img = None
-            img_match = re.search(r"(http(s?):)([/|.\w\s-])*\.(?:jpe?g|gif|png|webp)", str(entry))
-            if img_match is not None:
-                img = img_match.group(0)
-            obj = RssMessage(
-                bot=self.bot,
-                feed=FeedObject.unrecorded("web", channel.guild.id if channel.guild else None, channel.id, url),
-                url=link,
-                title=title,
-                date=entry_date,
-                entry_id=entry_id,
-                author=author,
-                channel=feed.feed["title"] if "title" in feed.feed else '?',
-                image=img,
-                post_text=post_text,
-                post_description=post_description
-            )
+            obj = await self._parse_entry(entry, feed, url, entry_date, channel)
             posts_list.append(obj)
         posts_list.reverse()
         return posts_list
