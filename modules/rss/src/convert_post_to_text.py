1a456d7fe5d8acb3f62cc3ed07aba437d1e49720
ZRunner <z.runner.mc@gmail.com>
2024-10-12 15:48:32 +0200

    feat(rss): add conversion from XHTML + convert html to markdown

--- modules/rss/src/convert_post_to_text.py
+++ modules/rss/src/convert_post_to_text.py
@@ -1,13 +1,21 @@
 import logging
 
 from bs4 import BeautifulSoup
+from markdownify import MarkdownConverter
 
 log = logging.getLogger("bot.rss")
+md_converter = MarkdownConverter(
+    convert=["a", "blockquote", "br", "code", "hn", "hr", "img", "list", "li", "p", "pre", "caption"]
+)
 
 async def get_text_from_entry(entry: dict) -> str | None:
     "Get the text from an RSS feed entry"
     if "content" in entry:
-        return await convert_post_to_text(entry["content"][0]["value"], entry["content"][0]["type"])
+        for content in entry["content"]:
+            if content["type"] in ["text/plain", "text/html", "application/xhtml+xml"]:
+                return await convert_post_to_text(content["value"], content["type"])
+        log.error("Unknown post type: %s", ', '.join(content["type"] for content in entry["content"]))
+        return None
     if "summary" in entry:
         if "summary_detail" not in entry:
             return entry["summary"]
@@ -24,18 +32,24 @@ async def get_summary_from_entry(entry: dict) -> str | None:
     log.warning("No summary in entry (or summary is same than content) for article %s", entry["link"])
     return None
 
+
 async def convert_post_to_text(post: str, post_type: str) -> str | None:
     "Convert an RSS feed post text into a plain text string"
+    if post_type == "text/plain":
+        return post
     if post_type == "text/html":
         return await convert_html_to_text(post)
-    elif post_type == "text/plain":
-        return post
-    else:
-        log.error("Unknown post type: %s", post_type)
-        return None
-
+    if post_type == "application/xhtml+xml":
+        return await convert_xhtml_to_text(post)
+    log.error("Unknown post type: %s", post_type)
+    return None
 
 async def convert_html_to_text(html: str) -> str:
     "Convert an HTML string into a plain text string"
     soup = BeautifulSoup(html, "html.parser")
-    return soup.get_text()
+    return md_converter.convert_soup(soup)
+
+async def convert_xhtml_to_text(html: str) -> str:
+    "Convert an XML string into a plain text string"
+    soup = BeautifulSoup(html, "lxml")
+    return md_converter.convert_soup(soup)
