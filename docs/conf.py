0687a26b49e413b6a6ad1d79f3c796e0905fbbb3
ZRunner <z.runner.mc@gmail.com>
2024-11-30 10:52:49 +0100

    feat(doc): bump version to 5.0.3

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ author = "ZRunner"
 # The short X.Y version
 version = "5.0"
 # The full version, including alpha/beta/rc tags
-release = "5.0.2"
+release = "5.0.3"
 
 _documentation_name = "Axobot Documentation"
 
