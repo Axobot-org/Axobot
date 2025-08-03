b6a035b78f29104b871bd5d2a13f5e5eb57b5c01
ZRunner <z.runner.mc@gmail.com>
2025-08-03 13:25:19 +0200

    fix(moderation): discord invite regex

--- core/bot_classes/consts.py
+++ core/bot_classes/consts.py
@@ -6,8 +6,8 @@ PRIVATE_GUILD_ID = discord.Object(625316773771608074)
 SUPPORT_GUILD_ID = discord.Object(356067272730607628)
 
 DISCORD_INVITE_REGEX = re.compile(
-    r'(?:https?://)?(?:www[.\s])?((?:discord[.\s](?:gg|io|li(?:nk)?)|discord\.me|discordapp\.com/invite\
-        |discord\.com/invite|dsc\.gg)[/\s]{1,3}[\w-]{2,27}(?!\w))'
+    r'(?:https?://)?(?:www[.\s])?((?:discord[.\s](?:gg|io|li(?:nk)?)|discord\.me|discordapp\.com/invite'
+    r'|discord\.com/invite|dsc\.gg)[/\s]{1,3}[\w-]{2,27}(?!\w))'
 )
 
 IGNORED_GUILDS = [
