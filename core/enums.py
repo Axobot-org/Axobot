bdfe05658241d0148bd8d289805d19c5b54d9b4d
ZRunner <z.runner.mc@gmail.com>
2026-03-31 22:39:17 +0200

    feat(events): add april 2026 card and event

--- core/enums.py
+++ core/enums.py
@@ -38,7 +38,8 @@ class RankCardsFlag(_BaseFlagClass):
         1 << 16: "halloween24",
         1 << 17: "christmas24",
         1 << 18: "april25",
-        1 << 19: "christmas25"
+        1 << 19: "christmas25",
+        1 << 20: "april26",
     }
 
 class UserFlag(_BaseFlagClass):
